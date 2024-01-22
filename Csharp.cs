3ad919c34a5149125bc320e9d5d528b0c14f3061
Omnia <omniaakhalill@gmail.com>
2024-01-22 15:37:43 +0200

    1 to swift

--- Csharp.cs
+++ Csharp.cs
@@ -9,3 +9,8 @@ namespace HelloWorld
     }
 
 }    
+
+
+{"name":"Iphone", "description":"new phone from apple", "price":"30000", "review":[{"name": "omnia", "rate":4.5 , "comment": "very good"},{"name": "david", "rate":0.5 , "comment": "baad"},{"name": "adly", "rate":4 , "comment": "nice"}]},
+
+{"name":"Samsung glaxy", "description":"new phone from samsun", "price":"20000", "review":[{"name": "mariem", "rate":4 , "comment": "good camera"},{"name": "mohamed", "rate":3 , "comment": "not bad"},{"name": "yasmeen", "rate":5 , "comment": "recomended"}]}
--- HelloSwift.c
+++ HelloSwift.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main() {
-   printf("HelloSwift");
+   printf("HelloSwift1");
+
    return 0;
 }
